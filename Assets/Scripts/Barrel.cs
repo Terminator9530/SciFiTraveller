6d2e13c51bead0dfaa53038594c9c0066f683a4d
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-09-06 16:13:22 +0530

    updated enemy damage and enemy health bar

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    public GameObject explosion;
+    public float destroyTime = 2f;
+    public float explosionRadius = 5f;
+    public int damage = 5;
+    public LayerMask layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            BlastBarrel();
+        }
+    }
+
+    public void BlastBarrel()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius, layerMask);
+        foreach (Collider2D hitCollider in hitColliders)
+        {
+            Debug.Log("Collider : " + hitCollider.name);
+            if (hitCollider.CompareTag("Enemy"))
+                hitCollider.GetComponent<SpaceShip>().ApplyDamage(damage);
+
+            if (hitCollider.CompareTag("Player"))
+            {
+                if (hitCollider.GetComponent<PlayerControls>() != null)
+                {
+                    hitCollider.GetComponent<PlayerControls>().ApplyDamage(damage);
+                }
+
+                if (hitCollider.GetComponent<AeroplaneController>() != null)
+                {
+                    hitCollider.GetComponent<AeroplaneController>().ApplyDamage(damage);
+                }
+            }
+        }
+
+        StartCoroutine(DestroyBarrel());
+    }
+
+    IEnumerator DestroyBarrel()
+    {
+        yield return new WaitForSeconds(destroyTime);
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.75F);
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
---
+++ Assets/Scripts/SawSpin.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawSpin : MonoBehaviour
+{
+    private Transform saw;
+    private int angle = 0;
+    public int damage = 1;
+    private bool isDamaging = false;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        saw = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        saw.transform.Rotate(saw.rotation.x, saw.rotation.y, angle, Space.Self);
+        angle += 3;
+        if (isDamaging)
+        {
+            if(player.GetComponent<PlayerControls>() != null)
+            {
+                player.GetComponent<PlayerControls>().ApplyDamage(damage);
+            }
+
+            if (player.GetComponent<AeroplaneController>() != null)
+            {
+                player.GetComponent<AeroplaneController>().ApplyDamage(damage);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            isDamaging = true;
+            player = collision.gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isDamaging = false;
+        }
+    }
+}
