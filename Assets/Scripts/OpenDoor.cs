eb4f8309f71b97c3c8ccfbf2b858dd3068fdd811
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-09-11 15:42:25 +0530

    updated ufo attractor and sci fi door

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject doorUnlocked;
+    public GameObject doorOpen;
+    private bool isTriggered = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTriggered && Input.GetKeyDown(KeyCode.E))
+        {
+            doorOpen.SetActive(true);
+            doorUnlocked.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTriggered = true;
+            canvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTriggered = false;
+            canvas.SetActive(false);
+        }
+    }
+}
