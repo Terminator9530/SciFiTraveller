7046989e279abbe4b4b3ac2a4eade43c1c6de39b
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-09-17 15:21:36 +0530

    updated responsiveness and touch controls

---
+++ Assets/Scripts/VirtualJoystickManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualJoystickManager : MonoBehaviour
+{
+    public GameObject virtualJoystick;
+    public GameObject pauseMenuButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+		showJoystickWhenAppropriate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void ShowJoystick()
+	{
+		virtualJoystick.SetActive(true);
+		pauseMenuButton.SetActive(true);
+	}
+
+	public void HideJoystick()
+	{
+		virtualJoystick.SetActive(false);
+		pauseMenuButton.SetActive(false);
+	}
+
+	private void showJoystickWhenAppropriate()
+	{
+		Debug.Log(Application.platform);
+		switch (Application.platform)
+		{
+			case RuntimePlatform.WindowsPlayer:
+			case RuntimePlatform.OSXPlayer:
+			case RuntimePlatform.LinuxPlayer:
+			case RuntimePlatform.WebGLPlayer:
+				HideJoystick();
+				Debug.Log("PC");
+				break;
+
+			case RuntimePlatform.Android:
+			case RuntimePlatform.IPhonePlayer:
+				ShowJoystick();
+				Debug.Log("Android");
+				break;
+
+			default:
+				Debug.Log("No plat");
+				break;
+		}
+	}
+}
